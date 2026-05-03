7f65e8bacd625827b79066603f92551b314cc74f
Jaron <dsz94235@gmail.com>
2026-05-03 20:37:45 +0800

    test: S2

--- src/sync/worker.ts
+++ src/sync/worker.ts
@@ -1,7 +1,2 @@
-import { Queue } from "./queue.js";
-const retry = require("retry-lib");
+export const worker = () => 's2-policy-tamper';
 
-export async function runWorker(): Promise<void> {
-  const q = new Queue();
-  await q.process();
-}
